8b78f6d910a878dea351e4a4f18fbc54aa2b5315
tony <antoabrand@gmail.com>
2018-01-08 14:35:19 -0600

    made ajax call update state

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 React Native with Redux - Practice project for learning React Native
 
+-- Notes
+    Diff between state and props
+        state - component's internal record keeping - only for class based components and not functional components
+
+        props - when wanting to communicate from a parent component to a child component
--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -26,12 +26,16 @@ class AlbumList extends Component {
 
         //returns a promise
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => this.setState({ albums: response.data }));
+            .then(
+                response => this.setState({ albums: response.data })
+            );
         //setState is given to us by Component and is availble to all class style components
         //and is the only way to update a component's state
     }
 
     render() {
+        console.log(this.state);
+
         return (
             <View>
                 <Text>Album List</Text>
