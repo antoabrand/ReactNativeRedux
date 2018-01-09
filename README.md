88862b9ccfd5a5994ac56c81eca74a3204235d18
tony <antoabrand@gmail.com>
2018-01-09 17:14:05 -0600

    adding images and styling

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ React Native with Redux - Practice project for learning React Native
         state - component's internal record keeping - only for class based components and not functional components
 
         props - when wanting to communicate from a parent component to a child component
+
+    For layouts - 
+        Flex box layout mechanism - used to layout multpiple items within a container
--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,16 +1,71 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image, Dimensions } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image } = album;
+    const { headerContenStyle, 
+            thumbnailStyle, 
+            thumbnailContainerStyle, 
+            headerTextStyle, 
+            albumCoverStyle } = styles;
+
     return (
         <Card>
             <CardItem>
-                <Text>{props.album.title}</Text>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContenStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardItem>
+
+            <CardItem>
+                <View>
+                    <Image 
+                        style={albumCoverStyle} 
+                        source={{ uri: image }} 
+                    />
+                </View>
             </CardItem>
         </Card>
+
     );
+};
+
+let win = Dimensions.get('window');
+
+const styles = {
+    headerContenStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        marginLeft: 10
+    },
+    headerTextStyle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    albumCoverStyle: {
+        height: 300,
+        width: win.width,
+        justifyContent: 'center'
+    }
 };
 
 export default AlbumDetail;  
