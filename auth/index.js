c397556f99509430743e60a4c2081c93e2b484a5
tony <antoabrand@gmail.com>
2018-01-20 13:31:38 -0600

    refactoring to eliminate many import and created an index.js to house

--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,5 @@
 import { AppRegistry, View, Text } from 'react-native';
 import React from 'react';
-import App from './src/components/App';
+import App from './src/App';
 
 AppRegistry.registerComponent('auth', () => App);
--- auth/src/App.js
+++ auth/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/commons';
 
 class App extends Component {
 
     render() {
         return (
             <View>
-                <Text>Hello App</Text>
+                <Header  headerText={'Authenticationgj'}/>
             </View>
         )
     }
