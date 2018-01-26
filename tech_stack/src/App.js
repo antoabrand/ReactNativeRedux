fa2492a5139a019ff440a1cbbb67984d38758aba
tony <antoabrand@gmail.com>
2018-01-26 11:37:51 -0600

    adding better package.json

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -11,9 +11,6 @@ class App extends Component {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-         
-         
-         
           <Header headerText="Tech Stack" />
           <LibraryList />
         </View>
