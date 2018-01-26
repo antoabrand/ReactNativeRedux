93363f39095306fd9f4eb88b6afa676ada8926d9
tony <antoabrand@gmail.com>
2018-01-26 11:06:42 -0600

    wired all the items to show the libraries on the view

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
@@ -10,10 +10,10 @@ class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <ScrollView>
+        <View>
           <Header headerText="Tech Stack" />
           <LibraryList />
-        </ScrollView>
+        </View>
       </Provider>
     );
   }
--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
-import { Alert } from "react-native";
+import { ListView } from "react-native";
 import { connect } from "react-redux";
+import ListItem from "./ListItem";
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: () => (r1, r2) => r1 != r2,
+    });
+
+    this.DataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(library) {
+    return <ListItem item={library} />;
+  }
   render() {
-    return true;
+    return <ListView dataSource={this.DataSource} renderRow={this.renderRow} />;
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state);
+  return { libraries: state.libraries };
 };
+
+//connect() will wire the mapStateToProps func to the component libraryList giving
+//access to the data the libraryList returns through the props system.
 
 export default connect(mapStateToProps)(LibraryList);
