5e12811807b995fcbd1495a8b546b9760a4818af
tony <antoabrand@gmail.com>
2018-01-26 09:25:10 -0600

    prettier

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
-import { Header } from './components/common';
-
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
+import { Header } from "./components/common";
+import LibraryList from "./components/LibraryList";
 
 class App extends Component {
-
     render() {
         return (
             <Provider store={createStore(reducers)}>
                 <ScrollView>
-                    <Header headerText='Tech Stack' />
+                    <Header headerText="Tech Stack" />
+                    <LibraryList />
                 </ScrollView>
             </Provider>
         );
     }
-
 }
 
-export default App; 
+
+export default App;
--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,10 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Alert } from "react-native";
+import { connect } from "react-redux";
 
 class LibraryList extends Component {
-
     render() {
-        return;
+        return true;
     }
 }
 
-export default LibraryList;
+const mapStateToProps = state => {
+    console.log(state);
+};
+
+export default connect(mapStateToProps)(LibraryList);
