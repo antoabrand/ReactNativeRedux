b69c79d55b1d1ebf49449a82caf615fe3d08a6a2
tony <antoabrand@gmail.com>
2018-01-30 14:01:32 -0600

    wiring of actions to expand card if selected

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -5,16 +5,31 @@ import { CardSection } from "./common";
 import * as actions from "../actions";
 
 class ListItem extends Component {
+  showExpanded() {
+    if (this.props.item.id === this.props.selectedLibraryId) {
+      console.log("inside ShowExpanded");
+      return (
+        <CardSection>
+          <Text style={styles.titleStyle}>{this.props.item.description}</Text>
+        </CardSection>
+      );
+    }
+  }
+
   render() {
     // console.log(this.props.item);
     const { titleStyle } = styles;
-    const {id, title} = this.props.item;
-    
+    const { id, title, description } = this.props.item;
+
     return (
+      //onPress give the id of th selected item to the action's payload
+      //action will then be used for the reducer to change the state and return the updated
+      //state with the new value
       <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
         <View>
           <CardSection>
             <Text style={titleStyle}>{title}</Text>
+            {this.showExpanded()}
           </CardSection>
         </View>
       </TouchableWithoutFeedback>
@@ -30,10 +45,14 @@ const styles = {
   cardStyle: {},
 };
 
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
 //first argument of connect is for mapStateToProprs
 //since we don't need mapStateToProps we are going to pass null as the first argument
 //the second argument will be to wire the actions object to listItem component as props
 //in order for it to have access to those actions - placing actions as the second argument
 //mutates the object to have the return action dispatched to the redux store - and takes all the actions
 //inside the object and passes them all to our component as props
-export default connect(null, actions)(ListItem);
+export default connect(mapStateToProps, actions)(ListItem);
--- tech_stack/src/reducers/SelectionReducer.js
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -1,4 +1,10 @@
-export default (state, action) => {
-  console.log(action);
-  return null;
+export default (state = null, action) => {
+  switch (action.type) {
+    case "select_library":
+      return action.payload;
+    default:
+      return state;
+  }
 };
+
+//reducers must always return something - if not type is met in the switch case then return the previously returned state
