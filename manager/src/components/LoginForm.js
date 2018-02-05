b11bee52cbd95247becb0f42ae456d3d25a6d8c1
tony <antoabrand@gmail.com>
2018-02-04 22:41:48 -0600

    added some stuff

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import { Card, CardSection, Input, Button } from "./common";
-import { emailChanged, passwordChanged, loginUser } from "../actions";
+import {
+  emailChanged,
+  passwordChanged,
+  loginUser,
+  loginUserFail
+} from "../actions";
 import { connect } from "react-redux";
+import { View, Text } from "react-native";
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -17,6 +23,15 @@ class LoginForm extends Component {
     this.props.loginUser({ email, password });
   }
 
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: "white" }}>
+          <Text style={styles.errorMessage}>{this.props.error}</Text>
+        </View>
+      );
+    }
+  }
   render() {
     return (
       <Card>
@@ -37,6 +52,9 @@ class LoginForm extends Component {
             value={this.props.password}
           />
         </CardSection>
+
+        {this.renderError()}
+
         <CardSection>
           <Button onPress={this.onLoginPress.bind(this)}>Login</Button>
         </CardSection>
@@ -44,17 +62,27 @@ class LoginForm extends Component {
     );
   }
 }
-//this is the func that will return what you need from state and pass them as props to your component to use
+
+const styles = {
+  errorMessage: {
+    alignSelf: "center",
+    color: "red",
+    fontSize: 20
+  }
+};
+//this is the func that will return props from state and pass them to your component
 const mapStateToProps = state => {
-  const { email, password } = state.auth;
+  const { email, password, error } = state.auth;
   return {
     email: email,
-    password: password
+    password: password,
+    error: error
   };
 };
 
 export default connect(mapStateToProps, {
   emailChanged,
   passwordChanged,
-  loginUser
+  loginUser,
+  loginUserFail
 })(LoginForm);
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -2,13 +2,16 @@
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS
+  LOGIN_USER,
+  LOGIN_USER_FAIL
 } from "../actions/actionConsts";
+import LoginForm from "../components/LoginForm";
 
 const INITIAL_STATE = {
-  email: " ",
-  password: " ",
-  user: " "
+  email: "",
+  password: "",
+  user: null,
+  error: ""
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -19,8 +22,10 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
-    case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload };
+    case LOGIN_USER:
+      return { ...state, user: action.payload, error: null };
+    case LOGIN_USER_FAIL:
+      return { ...state, error: "Authentication failed", password: "" };
     default:
       return state;
   }
